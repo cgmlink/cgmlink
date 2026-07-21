409878895bfa48255d3c2b6b5d253710c7c29fcc
Joseph Sawyer <joseph@jcsawyer.com>
2026-07-21 13:29:45 +0100

    feat: mark ingredient and meal entities soft deletable

--- src/CgmLink.Data.Migrators.MSSQL/Migrations/20260721122811_SoftDelete.Designer.cs
+++ src/CgmLink.Data.Migrators.MSSQL/Migrations/20260721122811_SoftDelete.Designer.cs
@@ -4,6 +4,7 @@ using CgmLink.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CgmLink.Data.Migrators.MSSQL.Migrations
 {
     [DbContext(typeof(CgmLinkDbContext))]
-    partial class CgmLinkDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260721122811_SoftDelete")]
+    partial class SoftDelete
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
