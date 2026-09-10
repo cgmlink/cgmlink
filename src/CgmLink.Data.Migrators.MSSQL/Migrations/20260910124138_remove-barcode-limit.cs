c8bed198ad92121608b7521677d963721beeb03d
AlexS <alexsawyer95@hotmail.com>
2026-09-10 13:42:01 +0100

    migration

---
+++ src/CgmLink.Data.Migrators.MSSQL/Migrations/20260910124138_remove-barcode-limit.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CgmLink.Data.Migrators.MSSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class removebarcodelimit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Barcode",
+                table: "ingredients",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(64)",
+                oldMaxLength: 64,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Barcode",
+                table: "ingredients",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+        }
+    }
+}
