220e2f485bdf385ff24dbc3d90cc50614976607e
AlexS <alexsawyer95@hotmail.com>
2026-09-22 20:05:14 +0100

    gate swagger behind config

---
+++ src/CgmLink.Api/Models/SwaggerSettings.cs
@@ -0,0 +1,9 @@
+namespace CgmLink.Api.Models;
+
+public class SwaggerSettings
+{
+    public const string SectionName = "Swagger";
+
+    public bool Enabled { get; set; }
+    public bool UI { get; set; }
+}
